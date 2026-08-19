a360d685640c58457ab107f465d0eac714227664
SajjadKhanYousafzai <sk4512949@gmail.com>
2026-08-19 20:23:57 +0500

    fix(deploy): configure automatic basePath and assetPrefix for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let basePath = '';
+let assetPrefix = '';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY ? process.env.GITHUB_REPOSITORY.replace(/.*?\//, '') : '';
+  if (repo) {
+    basePath = `/${repo}`;
+    assetPrefix = `/${repo}/`;
+  }
+}
+
 const nextConfig = {
   output: 'export',
+  basePath: basePath || undefined,
+  assetPrefix: assetPrefix || undefined,
+  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
